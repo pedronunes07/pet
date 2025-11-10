f8a24fc4ec8e3604a9b5063df0df732ba1124c71
pedronunes07 <148357835+pedronunes07@users.noreply.github.com>
2025-11-10 14:10:57 -0300

    Delete sistema-academico directory

--- sistema-academico/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { PetForm } from './pet/pet-form/pet-form';
-import { PetList } from './pet/pet-list/pet-list';
-
-export const routes: Routes = [
-    { path: '', component: PetList },
-    { path: 'novo', component: PetForm }
-];
